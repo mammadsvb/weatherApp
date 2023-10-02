7128e8067e3360a30e7ea01ebe63ee25b82e621d
Mohammad <>
2023-10-02 15:58:02 +0330

    init ejs

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ const axios = require("axios");
 
 const app = express();
 
+app.use(express.static('public'));
+app.set("view engine",'ejs');
+
 if(process.env.NODE_ENV != "production")
     require("dotenv").config();
 
-
+app.get('/',(req,res)=>{
+    res.render('main');
+})
 
 const port = process.env.PORT || 3000;
 
