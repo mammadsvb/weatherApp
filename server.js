c78210471538e6bb93cba960637def1ac7c0e74d
Mohammad <>
2023-10-04 12:56:05 +0330

    sample get data

--- server.js
+++ server.js
@@ -9,6 +9,24 @@ app.set("view engine",'ejs');
 if(process.env.NODE_ENV != "production")
     require("dotenv").config();
 
+let location = "london";
+const url = `http://api.weatherapi.com/v1/current.json?key=${process.env.API_KEY}&q=${location}&aqi=no`
+
+async function wheather(url){
+    try{
+        const response = await axios.get(url);
+        console.log(response.data)
+        const data = {location:{name:response.data.location.name,country:response.data.location.name}}
+
+    }catch(e){
+        console.error(e.response.data)
+    }  
+}
+
+wheather(url);
+
+
+
 app.get('/',(req,res)=>{
     res.render('main');
 })
